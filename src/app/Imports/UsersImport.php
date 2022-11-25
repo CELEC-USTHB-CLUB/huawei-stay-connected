57b46536f8e2b75713ef60c53de2f957dadf6d72
LAGGOUNE Walid <walidlaggoune159@gmail.com>
2022-11-25 12:15:55 +0100

    fixed importer 2

--- src/app/Imports/UsersImport.php
+++ src/app/Imports/UsersImport.php
@@ -8,19 +8,21 @@ use Maatwebsite\Excel\Concerns\ToModel;
 class UsersImport implements ToModel
 {
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
-        return new User([
-            'fullname' => $row[0],
-            'email' => $row[1],
-            'study_level' => $row[2],
-            'specialite' => $row[3],
-            'is_usthb' => $row[4],
-            'is_celec' => $row[5],
-        ]);
+        if ($row[0] !== null) {
+            return new User([
+                'fullname' => $row[0],
+                'email' => $row[1],
+                'study_level' => $row[2],
+                'specialite' => $row[3],
+                'is_usthb' => $row[4],
+                'is_celec' => $row[5],
+            ]);
+        }
     }
 }
